c1f15e200f228cfb2d4b67e6f5d2017cf9e0ec67
Tempest042 <brendan.pert@gmail.com>
2016-09-09 14:35:42 +1000

    Add files via upload

---
+++ Battleships-Xamarin/src/Model/AIOption.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualBasic;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+// The different AI levels.
+public enum AIOption
+{
+	// Easy, total random shooting
+	Easy,
+
+	// Medium, marks squares around hits
+	Medium,
+
+	// As medium, but removes shots once it misses
+	Hard
+}
