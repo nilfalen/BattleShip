811dde90c9b7d995250fb2d988bf963e1484ddd3
undermoment <cezheng8@gmail.com>
2016-10-07 15:32:59 +1100

    Add files via upload

---
+++ Battleships-Xamarin/src/GameControllerUnitTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+namespace MyGame
+{
+	[TestFixture]
+	public class GameControllerUnitTest
+	{
+		[Test]
+		public void StartGameTest () 
+		{
+			GameController.StartGame ();
+			Assert.IsNotNull (GameController.HumanPlayer);
+		}
+		 
+	}
+}
