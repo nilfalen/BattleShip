969ba2e99e3de6a2e0c26b525aa61e8bc4ca1786
nilfalen <nilfalen1@gmail.com>
2016-10-21 11:08:15 +1100

    Add files via upload

--- Battleships-Xamarin/src/HighScoreController.cs
+++ Battleships-Xamarin/src/HighScoreController.cs
@@ -175,7 +175,7 @@ static class HighScoreController
 			int x = 0;
 			x = SCORES_LEFT + SwinGame.TextWidth(GameResources.GameFont("Courier"), "Name: ");
 
-			SwinGame.StartReadingText(Color.White, NAME_WIDTH, GameResources.GameFont("Courier"), x, ENTRY_TOP);
+			SwinGame.StartReadingTextWithText("AAA", Color.White, NAME_WIDTH+1, GameResources.GameFont("Courier"), x, ENTRY_TOP);
 
 			//Read the text from the user
 			while (SwinGame.ReadingText()) {
@@ -190,7 +190,9 @@ static class HighScoreController
 			s.Name = SwinGame.TextReadAsASCII();
 
 			if (s.Name.Length < 3) {
-				s.Name = s.Name + new string(Convert.ToChar(" "), 3 - s.Name.Length);
+				s.Name = s.Name + new string (Convert.ToChar (" "), 3 - s.Name.Length);
+			} else if (s.Name.Length > 3) {
+				s.Name = s.Name.Remove (3);
 			}
 
 			_Scores.RemoveAt(_Scores.Count - 1);
